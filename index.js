2654f43e8c17a3cf7caad06fb766087b3cd7f925
Ajaykumar <123239254+ajaykumarn9000@users.noreply.github.com>
2024-11-10 20:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ export const handler = async (event) => {
   };
   return response;
 };
+
+// Lambda S3 Bucket
+import json
+def lambda_handler(event, context):
+    # Extract bucket name and object key from the event
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+       
+        # Log the message
+        print(f"An object has been added to the bucket {bucket_name}: {object_key}")
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Log completed')
+    }
